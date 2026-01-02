2422b095310e1385b7b40a727d7cae18b74f329a
orann11 <clippingsipping@gmail.com>
2026-01-02 15:27:32 +0300

    Adjusted Visualizer

--- src/main/java/Interface/Visualizer.java
+++ src/main/java/Interface/Visualizer.java
@@ -2,15 +2,24 @@ package Interface;
 
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Visualizer extends JFrame{
 
     public Visualizer(String title, int width, int height){
         super(title.isEmpty() ? "Visualizer" : title);
         this.setSize(width, height);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Terminate program when clicked on 'x'
     }
+
     public void display(){
         this.setVisible(true);
     }
+
+    public void addComponent(Component c){
+        this.add(c);
+    }
+
 
 }
