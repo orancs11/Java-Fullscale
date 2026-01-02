24ec887f404d88d74811cdfcf4517f9d454d573f
orann11 <clippingsipping@gmail.com>
2026-01-02 13:44:19 +0300

    Selection sort and swap function is implemented. Display function is moved inside Sorter singleton

--- src/main/java/oran/App.java
+++ src/main/java/oran/App.java
@@ -9,22 +9,14 @@ public class App
     public static void main( String[] args ) {
         int[] numbers = new int[]{5, 2, 3, 4, 1, 8, 2, 12, 3, 15};
         System.out.println("Original version");
-        display(numbers);
-        Sorter.bubbleSort(numbers);
+        Sorter.display(numbers);
+
+        //Sorter.bubbleSort(numbers); Bubble Sort
+        Sorter.selectionSort(numbers);
         System.out.println("Sorted version");
-        display(numbers);
+        Sorter.display(numbers);
+
 
 
     }
-
-    public static void display(int[] arr){
-        String message = "";
-        for(int i = 0; i < arr.length; i++){
-            if(i == arr.length - 1) message = String.valueOf(arr[i]);
-            else message = arr[i] + ", ";
-            System.out.print(message);
-        }
-        System.out.println();
-    }
-
 }
--- src/main/java/oran/Sorter.java
+++ src/main/java/oran/Sorter.java
@@ -3,14 +3,35 @@ package oran;
 public class Sorter {
     public static void bubbleSort(int[] arr){
         int size = arr.length;
-        for(int i = 0; i < size; i++){
-            for(int j = 0; j < size - 1; j++){
-                if(arr[j] > arr[j + 1]){
-                    int temp =  arr[j + 1];
-                    arr[j + 1] = arr[j];
-                    arr[j] = temp;
-                }
-            }
+        for(int i = 0; i < size; i++)
+            for(int j = 0; j < size - 1; j++)
+                if(arr[j] > arr[j + 1]) swap(arr, i, j);
+    }
+
+    public static void display(int[] arr){
+        String message = "";
+        for(int i = 0; i < arr.length; i++){
+            if(i == arr.length - 1) message = String.valueOf(arr[i]);
+            else message = arr[i] + ", ";
+            System.out.print(message);
         }
+        System.out.println();
+    }
+
+    public static void selectionSort(int[] arr){
+        int size = arr.length;
+
+
+        for(int i = 0; i < size; i++){
+            int minIndex = i;
+            for(int j = i + 1; j < size - 1; j++)
+                if(arr[j] < arr[minIndex]) minIndex = j;
+            swap(arr, i, minIndex);
+        }
+    }
+    private static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
