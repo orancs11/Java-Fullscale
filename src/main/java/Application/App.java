93aa866aeee2913fec89938bceebb939c8f31bdf
orann11 <clippingsipping@gmail.com>
2026-01-04 16:12:21 +0300

    Bar class is updated

--- src/main/java/Application/App.java
+++ src/main/java/Application/App.java
@@ -1,9 +1,11 @@
 package Application;
 
+import Interface.Bar;
 import Interface.BarPanel;
 import Interface.Visualizer;
 import Logic.Sorter;
 import javax.swing.*;
+import java.awt.*;
 
 /**
 
@@ -23,9 +25,20 @@ public class App
         System.out.println("Sorted version");
         Sorter.display(numbers);
 
-        Visualizer visualizer = new Visualizer("Visualizerr", 300, 400);
-        visualizer.display();
-        visualizer.createBarPlot(numbers);
+        //Visualizer visualizer = new Visualizer("Visualizerr", 300, 400);
+        //Bar bar = new Bar();
+        //visualizer.display();
+        //visualizer.add(bar);
+        //visualizer.createBarPlot(numbers);
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.BLACK);
+        JFrame frame = new JFrame("VISUALIZER");
+        frame.setSize(1920, 1080);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        //panel.add(new Bar(100, 200, 0, 0, Color.WHITE));
+        frame.getContentPane().add(panel, BorderLayout.CENTER);
+        frame.setVisible(true);
+
 
     }
 }
--- src/main/java/Interface/Bar.java
+++ src/main/java/Interface/Bar.java
@@ -5,15 +5,21 @@ import java.awt.Graphics;
 import java.awt.Color;
 
 public class Bar extends JComponent{
-
-    public Bar(){
+    private int width, height, x, y;
+    private Color color;
+    public Bar(int width, int height, int x, int y, Color color){
         super();
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        this.color = color;
     }
 
-    public void paintComponent(Graphics g, Color barColor, int x, int y, int width, int height){
+    public void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.setColor(barColor);
-        g.drawRect(x, y, width, height);
-        g.fillRect(x, y, width, height);
+        g.setColor(this.color);
+        g.drawRect(this.x, this.y, this.width, this.height);
+        g.fillRect(this.x, this.y, this.width, this.height);
     }
 }
