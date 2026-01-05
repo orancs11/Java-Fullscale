ca6be5cbe20536720c3becb46e2e843b8a19b11b
orann11 <clippingsipping@gmail.com>
2026-01-05 10:59:35 +0300

    Recroder is finished

--- src/main/java/Application/App.java
+++ src/main/java/Application/App.java
@@ -14,6 +14,8 @@ import java.awt.event.ActionListener;
 public class App
 {
     public static void main( String[] args ) {
+
+
         JFrame frame = new JFrame();
         JPanel inputPanel = new JPanel(new FlowLayout());
         JPanel upperPanel = new JPanel();
